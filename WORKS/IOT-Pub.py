65a71a8fd6a7fa6f4e8a03b6eca098a60cb1af77
bozkurthan <burakhancorak.ws@gmail.com>
2019-10-16 07:46:22 +0300

    Update IOT-Pub.py

--- WORKS/IOT-Pub.py
+++ WORKS/IOT-Pub.py
@@ -10,19 +10,29 @@ print("creating new instance")
 client = mqtt.Client("P1")  # create new instance
 print("connecting to broker")
 
-i = 0
-while i < 10:
-  publish_message = "( (L1-C1-0.Paket), (2019-01-20 16:56:58.792566), ( ( Data: (Light:33.141613,  Humidty:52.87946, Temperature:NaN ) ) )"
-  publish.single("test", publish_message, 1, False, "localhost", 1883)
-  i += 1
-  time.sleep(1)
 
-time.sleep(5)
+
 
 i = 0
-while i < 10:
-  publish_message = "(L1-C1-0.paket),(2019-01-20 16:56:58.792566),(33.141613,52.87946,36.59778)"
-  publish.single("test", publish_message, 1, False, "localhost", 1883)
+while i < 1000:
+  publish_message="(L1-C1-"+ str(i) + ".paket),["+ str(long(time.time() * 1000))+"],(33.141613,52.87946,36.59778)"
+  publish.single("test4646",publish_message,1,False,"localhost",1883)
+  publish_message="(L1-C2-"+ str(i) + ".paket),["+ str(long(time.time() * 1000))+"],(33.141613,52.87946,36.59778)"
+  publish.single("test4646",publish_message,1,False,"localhost",1883)
+  publish_message="(L1-C3-"+ str(i) + ".paket),["+ str(long(time.time() * 1000))+"],(33.141613,52.87946,36.59778)"
+  publish.single("test4646",publish_message,1,False,"localhost",1883)
+  publish_message="(L2-C1-"+ str(i) + ".paket),["+ str(long(time.time() * 1000))+"],(33.141613,52.87946,36.59778)"
+  publish.single("test4646",publish_message,1,False,"localhost",1883)
+  publish_message="(L2-C2-"+ str(i) + ".paket),["+ str(long(time.time() * 1000))+"],(33.141613,52.87946,36.59778)"
+  publish.single("test4646",publish_message,1,False,"localhost",1883)
+  publish_message="(L2-C3-"+ str(i) + ".paket),["+ str(long(time.time() * 1000))+"],(33.141613,52.87946,36.59778)"
+  publish.single("test4646",publish_message,1,False,"localhost",1883)
   i += 1
-  time.sleep(1)
+  time.sleep(0.2)
 
+#i = 0
+#while i < 10:
+#  publish_message = "( (L1-C1-0.Paket), (2019-01-20 16:56:58.792566), ( ( Data: (Light:33.141613,  Humidty:52.87946, Temperature:NaN ) ) )"
+#  publish.single("test", publish_message, 1, False, "localhost", 1883)
+#  i += 1
+#  time.sleep(1)
