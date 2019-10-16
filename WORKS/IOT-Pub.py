f0ee9ffd4f2275486d7a623add2bbec0c0bbd3f3
bozkurthan <burakhancorak.ws@gmail.com>
2019-10-16 13:26:10 +0300

    Update IOT-Pub.py
    Signed-off-by: bozkurthan <burakhancorak.ws@gmail.com>

--- WORKS/IOT-Pub.py
+++ WORKS/IOT-Pub.py
@@ -4,6 +4,8 @@ import time
 
 ##MAIN CODE
 broker_address = "127.0.0.1"
+from numpy import double, long
+
 mesaj = "hello"
 # broker_address="iot.eclipse.org"
 print("creating new instance")
@@ -26,3 +28,106 @@ while i < 10:
   i += 1
   time.sleep(1)
 
+# 0 = Unprocessed data
+# 1 = Processed data
+# 2 = Both data
+pub_type = 2
+time_of_unproccessed = 0.2
+time_of_proccessed = 0.2
+time_both_data = 1
+pub_topic = "test"
+host = "localhost"
+qos_type = 1
+
+if (pub_type == 0):
+  i = 0
+  while i < 10:
+    publish_message = "( (L1-C1-" + str(i) + ".paket), [" + str(
+      long(time.time() * 1000)) + "], ( ( Data: (Light:33.141613,  Humidty:52.87946, Temperature:NaN ) ) )"
+    publish.single(pub_topic, publish_message, qos_type, False, host, 1883)
+    publish_message = "( (L1-C2-" + str(i) + ".paket), [" + str(
+      long(time.time() * 1000)) + "], ( ( Data: (Light:33.141613,  Humidty:NaN, Temperature:36.59778 ) ) )"
+    publish.single(pub_topic, publish_message, qos_type, False, host, 1883)
+    publish_message = "( (L1-C3-" + str(i) + ".paket), [" + str(
+      long(time.time() * 1000)) + "], ( ( Data: (Light:NaN,  Humidty:52.87946, Temperature:36.59778 ) ) )"
+    publish.single(pub_topic, publish_message, qos_type, False, host, 1883)
+    publish_message = "( (L2-C1-" + str(i) + ".paket), [" + str(
+      long(time.time() * 1000)) + "], ( ( Data: (Light:33.141613,  Humidty:52.87946, Temperature:NaN ) ) )"
+    publish.single(pub_topic, publish_message, qos_type, False, host, 1883)
+    publish_message = "( (L2-C2-" + str(i) + ".paket), [" + str(
+      long(time.time() * 1000)) + "], ( ( Data: (Light:33.141613,  Humidty:NaN, Temperature:36.59778 ) ) )"
+    publish.single(pub_topic, publish_message, qos_type, False, host, 1883)
+    publish_message = "( (L2-C3-" + str(i) + ".paket), [" + str(
+      long(time.time() * 1000)) + "], ( ( Data: (Light:NaN,  Humidty:52.87946, Temperature:36.59778 ) ) )"
+    publish.single(pub_topic, publish_message, qos_type, False, host, 1883)
+    i += 1
+    time.sleep(time_of_unproccessed)
+elif (pub_type == 1):
+  i = 0
+  while i < 10:
+    publish_message = "(L1-C1-" + str(i) + ".paket),[" + str(
+      long(time.time() * 1000)) + "],(33.141613,52.87946,36.59778)"
+    publish.single(pub_topic, publish_message, qos_type, False, host, 1883)
+    publish_message = "(L1-C2-" + str(i) + ".paket),[" + str(
+      long(time.time() * 1000)) + "],(33.141613,52.87946,36.59778)"
+    publish.single(pub_topic, publish_message, qos_type, False, host, 1883)
+    publish_message = "(L1-C3-" + str(i) + ".paket),[" + str(
+      long(time.time() * 1000)) + "],(33.141613,52.87946,36.59778)"
+    publish.single(pub_topic, publish_message, qos_type, False, host, 1883)
+    publish_message = "(L2-C1-" + str(i) + ".paket),[" + str(
+      long(time.time() * 1000)) + "],(33.141613,52.87946,36.59778)"
+    publish.single(pub_topic, publish_message, qos_type, False, host, 1883)
+    publish_message = "(L2-C2-" + str(i) + ".paket),[" + str(
+      long(time.time() * 1000)) + "],(33.141613,52.87946,36.59778)"
+    publish.single(pub_topic, publish_message, qos_type, False, host, 1883)
+    publish_message = "(L2-C3-" + str(i) + ".paket),[" + str(
+      long(time.time() * 1000)) + "],(33.141613,52.87946,36.59778)"
+    publish.single(pub_topic, publish_message, qos_type, False, host, 1883)
+    i += 1
+    time.sleep(time_of_proccessed)
+elif (pub_type == 2):
+  i = 0
+  while i < 10:
+    publish_message = "( (L1-C1-" + str(i) + ".paket), [" + str(
+      long(time.time() * 1000)) + "], ( ( Data: (Light:33.141613,  Humidty:52.87946, Temperature:NaN ) ) )"
+    publish.single(pub_topic, publish_message, qos_type, False, host, 1883)
+    publish_message = "( (L1-C2-" + str(i) + ".paket), [" + str(
+      long(time.time() * 1000)) + "], ( ( Data: (Light:33.141613,  Humidty:NaN, Temperature:36.59778 ) ) )"
+    publish.single(pub_topic, publish_message, qos_type, False, host, 1883)
+    publish_message = "( (L1-C3-" + str(i) + ".paket), [" + str(
+      long(time.time() * 1000)) + "], ( ( Data: (Light:NaN,  Humidty:52.87946, Temperature:36.59778 ) ) )"
+    publish.single(pub_topic, publish_message, qos_type, False, host, 1883)
+    publish_message = "( (L2-C1-" + str(i) + ".paket), [" + str(
+      long(time.time() * 1000)) + "], ( ( Data: (Light:33.141613,  Humidty:52.87946, Temperature:NaN ) ) )"
+    publish.single(pub_topic, publish_message, qos_type, False, host, 1883)
+    publish_message = "( (L2-C2-" + str(i) + ".paket), [" + str(
+      long(time.time() * 1000)) + "], ( ( Data: (Light:33.141613,  Humidty:NaN, Temperature:36.59778 ) ) )"
+    publish.single(pub_topic, publish_message, qos_type, False, host, 1883)
+    publish_message = "( (L2-C3-" + str(i) + ".paket), [" + str(
+      long(time.time() * 1000)) + "], ( ( Data: (Light:NaN,  Humidty:52.87946, Temperature:36.59778 ) ) )"
+    publish.single(pub_topic, publish_message, qos_type, False, host, 1883)
+    i += 1
+    time.sleep(time_of_unproccessed)
+  time.sleep(time_both_data)
+  i = 0
+  while i < 10:
+    publish_message = "(L1-C1-" + str(i) + ".paket),[" + str(
+      long(time.time() * 1000)) + "],(33.141613,52.87946,36.59778)"
+    publish.single(pub_topic, publish_message, qos_type, False, host, 1883)
+    publish_message = "(L1-C2-" + str(i) + ".paket),[" + str(
+      long(time.time() * 1000)) + "],(33.141613,52.87946,36.59778)"
+    publish.single(pub_topic, publish_message, qos_type, False, host, 1883)
+    publish_message = "(L1-C3-" + str(i) + ".paket),[" + str(
+      long(time.time() * 1000)) + "],(33.141613,52.87946,36.59778)"
+    publish.single(pub_topic, publish_message, qos_type, False, host, 1883)
+    publish_message = "(L2-C1-" + str(i) + ".paket),[" + str(
+      long(time.time() * 1000)) + "],(33.141613,52.87946,36.59778)"
+    publish.single(pub_topic, publish_message, qos_type, False, host, 1883)
+    publish_message = "(L2-C2-" + str(i) + ".paket),[" + str(
+      long(time.time() * 1000)) + "],(33.141613,52.87946,36.59778)"
+    publish.single(pub_topic, publish_message, qos_type, False, host, 1883)
+    publish_message = "(L2-C3-" + str(i) + ".paket),[" + str(
+      long(time.time() * 1000)) + "],(33.141613,52.87946,36.59778)"
+    publish.single(pub_topic, publish_message, qos_type, False, host, 1883)
+    i += 1
+    time.sleep(time_of_proccessed)
